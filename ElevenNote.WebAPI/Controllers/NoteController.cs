71427430a3a3835144150c3f504a3c81dd475757
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-17 19:39:47 -0400

    update method

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -35,11 +35,26 @@ namespace ElevenNote.WebAPI.Controllers
             return Ok();
         }
 
+        //Get by Id
         public IHttpActionResult Get(int id)
         {
             NoteService noteService = CreateNoteService();
             var note = noteService.GetNoteById(id);
             return Ok(note);
+        }
+
+        //Put or Update
+        public IHttpActionResult Put(NoteEdit note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.UpdateNote(note))
+                return InternalServerError();
+
+            return Ok();
         }
 
         private NoteService CreateNoteService()
