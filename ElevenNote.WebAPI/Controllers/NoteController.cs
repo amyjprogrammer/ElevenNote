24441e998c976e6d0dce67412fd2d06368f9b788
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-17 18:11:18 -0400

    added get and post option

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,46 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]//security layer
+    public class NoteController : ApiController
+    {
+        
+        //Get or Read
+        public IHttpActionResult Get()
+        {
+            NoteService noteService = CreateNoteService();
+            var notes = noteService.GetNotes();
+            return Ok(notes);
+        }
+
+        //Create or Put
+        public IHttpActionResult Post(NoteCreate note)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateNoteService();
+
+            if (!service.CreateNote(note))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var noteService = new NoteService(userId);
+            return noteService;
+        }
+        //ask on Monday why this was on the bottom and about [HttpPost] not being included
+    }
+}
