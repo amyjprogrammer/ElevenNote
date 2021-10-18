252991c3f849adb9f3940ff8fe13471c0e913693
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-18 17:42:33 -0400

    added classes to Category

---
+++ Category.Models/CategoryListItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Category.Models
+{
+    public class CategoryListItem
+    {
+        public int CategoryId { get; set; }
+
+        [Display(Name ="Category Title")]
+        public string CategoryTitle { get; set; }
+    }
+}
---
+++ Category.Services/CategoryCreate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Category.Services
+{
+    public class CategoryCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
+        [MaxLength(100, ErrorMessage ="Please only enter 100 characters.")]
+        public string CategoryTitle { get; set; }
+    }
+}
