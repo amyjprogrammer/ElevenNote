3637985abe5af97b5b90ae2bba4c15d1a4d10678
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-18 22:28:57 -0400

    CategoryService

--- Category.Services/CategoryService.cs
+++ Category.Services/CategoryService.cs
@@ -1,4 +1,6 @@
 ﻿using Category.Data;
+using Category.Models;
+using ElevenNote.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,8 +13,36 @@ namespace Category.Services
     {
         public bool CreateCategory(CategoryCreate model)
         {
-            var entity = new CategoryNew()
-                
+            var entity =
+                new CategoryNew()
+                {
+                    CategoryTitle = model.CategoryTitle
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Select(
+                            e =>
+                            new CategoryListItem
+                            {
+                                CategoryId = e.CategoryId,
+                                CategoryTitle = e.CategoryTitle
+                            }
+                        );
+                return query.ToArray();
+            }
         }
     }
+
 }
