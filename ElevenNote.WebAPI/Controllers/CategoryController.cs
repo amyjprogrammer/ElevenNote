aa43f296323a5521a3c942f152d17b56d0ed6577
amyjprogrammer <amyjprogrammer@gmail.com>
2021-10-18 22:55:55 -0400

    added the controller

---
+++ ElevenNote.WebAPI/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Category.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    public class CategoryController : ApiController
+    {
+
+        public IHttpActionResult Get()
+        {
+            var categoryService = CreateCategoryService();
+            var categories = categoryService.GetCategories();
+            return Ok(categories);
+        }
+
+        public IHttpActionResult Post(CategoryCreate category)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateCategoryService();
+
+            if (!service.CreateCategory(category))
+                return InternalServerError();
+
+            return Ok();
+        }
+        private CategoryService CreateCategoryService()
+        {
+            var categoryService = new CategoryService();
+            return categoryService;
+        }
+    }
+}
